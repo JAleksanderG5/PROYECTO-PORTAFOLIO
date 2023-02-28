bb793278b8b815de78e235764e8f908c5ca5f55a
JAleksanderG5 <jorge.garciam150@gmail.com>
2023-02-28 01:20:07 -0600

    Se realizaron las secciones de contacto y footer

--- script.js
+++ script.js
@@ -15,3 +15,35 @@
     $menu.classList.remove("is-active");
   });
 })(document);
+
+((d) => {
+  const $form = d.querySelector(".contacto-form"),
+    $response = d.querySelector(".contacto-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch("https://formsubmit.co/ajax/alexanderjorge150@gmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        location.hash = "#gracias";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+        setTimeout(() => {
+          location.hash = "#cerrar";
+        }, 3000);
+      });
+  });
+})(document);
